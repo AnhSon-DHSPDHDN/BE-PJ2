c672fa0e39bb43d5e8565819a90b405af793de45
AnhSon-DHSPDHDN <59197139+AnhSon-DHSPDHDN@users.noreply.github.com>
2021-07-20 09:37:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+app.use(auth)
+app.use(router)
+app.listen(9000)
