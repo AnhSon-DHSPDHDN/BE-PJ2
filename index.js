bbdff5273e9f206cd9092d67e7cd723e07523108
AnhSon-DHSPDHDN <59197139+AnhSon-DHSPDHDN@users.noreply.github.com>
2021-07-20 10:07:36 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
+const cors = require('cors')
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
+
+const corsOptions = {
+  origin: '*',
+}
+
+app.use(cors(corsOptions))
 
 app.use(auth)
 app.use(router)
